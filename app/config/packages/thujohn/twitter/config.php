2e194fd670fece9c47b3f8ca4cecdfa78ef41e5b
Devsprite <devsprite@gmail.com>
2014-09-13 11:16:26 +0200

    initial checkin

---
+++ app/config/packages/thujohn/twitter/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// You can find the keys here : https://dev.twitter.com/
+
+return array(
+	'API_URL'             => 'api.twitter.com',
+	'API_VERSION'         => '1.1',
+	'AUTHENTICATE_URL'    => 'https://api.twitter.com/oauth/authenticate',
+	'AUTHORIZE_URL'       => 'https://api.twitter.com/oauth/authorize',
+	'ACCESS_TOKEN_URL'    => 'oauth/access_token',
+	'REQUEST_TOKEN_URL'   => 'oauth/request_token',
+	'USE_SSL'             => true,
+
+	'CONSUMER_KEY'        => 'WUjUTNMVmImfmF4TNBo8pem2a',
+	'CONSUMER_SECRET'     => 'nsQz6JMQGt29cWu0FuIeiHodMeVS6QjqjAthAg6uIcfvAijyi1',
+	'ACCESS_TOKEN'        => '2395763472-RjryqqrC4UTqUEzsDqJpXa38z9EQzil1GbmesUD',
+	'ACCESS_TOKEN_SECRET' => 'ZP8dYzXTqElgNI5pRyylsT5PlSRJqJ8vD0zdGmOFECphi',
+);
